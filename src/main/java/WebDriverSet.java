7cd36f4e9cbb681861907e93912b689ef8021fb1
Alexey Sysoev <wowregfatgod@yandex.ru>
2023-01-19 00:17:27 +0300

    small changes WebDriverSet.java

--- src/main/java/WebDriverSet.java
+++ src/main/java/WebDriverSet.java
@@ -6,7 +6,6 @@ public class WebDriverSet {
     WebDriver driver;
     public WebDriver getWebDriver(){
         String browser = System.getProperties().getProperty("webbrowser");
-        System.out.println(browser);
         if ((browser == null) || (browser.equals("chrome"))){
             System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
             driver = new ChromeDriver();
@@ -14,7 +13,7 @@ public class WebDriverSet {
         else if (browser.equals("yandex")) {
             System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriverya106.exe");
             ChromeOptions options = new ChromeOptions();
-            options.setBinary("C:\\Users\\Alexoid\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
             driver = new ChromeDriver(options);
         }
         return driver;
