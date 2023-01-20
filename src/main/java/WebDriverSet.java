b03440e0bebc99e560cc190a3745a4196845b15e
Alexey Sysoev <wowregfatgod@yandex.ru>
2023-01-20 23:49:28 +0300

    small changes WebDriverSet.java

--- src/main/java/WebDriverSet.java
+++ src/main/java/WebDriverSet.java
@@ -3,19 +3,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverSet {
-    WebDriver driver;
-    public WebDriver getWebDriver(){
-        String browser = System.getProperties().getProperty("webbrowser");
-        if ((browser == null) || (browser.equals("chrome"))){
+    private WebDriver driver;
+    public WebDriver getWebDriver() {
+        String browser = System.getProperties().getProperty("browser");
+        if ((browser == null) || (browser.equals("chrome")) || (browser.length() != 0)) {
             System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
             driver = new ChromeDriver();
-        }
-        else if (browser.equals("yandex")) {
+        } else if (browser.equals("yandex")) {
             System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriverya106.exe");
             ChromeOptions options = new ChromeOptions();
             options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
             driver = new ChromeDriver(options);
         }
         return driver;
+        /*String browser = System.getProperties().getProperty("browser");
+        System.out.println(browser);
+        if (browser.equals("yandex")) {
+            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriverya106.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+            driver = new ChromeDriver(options);
+        }
+        else {
+            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        return driver;*/
     }
 }
+
