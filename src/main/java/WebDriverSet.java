3b69c8746af467bec0d6ee0ad509bf792195c2a1
Alexey Sysoev <wowregfatgod@yandex.ru>
2023-03-02 10:34:01 +0300

    trying run webdriver in jenkins

--- src/main/java/WebDriverSet.java
+++ src/main/java/WebDriverSet.java
@@ -6,16 +6,18 @@ public class WebDriverSet {
     private WebDriver driver;
 
     public WebDriver getWebDriver() {
-        String browser = System.getProperties().getProperty("browser");
-        if ((browser == null) || (browser.equals("chrome"))) {
-            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
-            driver = new ChromeDriver();
-        } else if (browser.equals("yandex")) {
-            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver108forya.exe");
-            ChromeOptions options = new ChromeOptions();
-            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
-            driver = new ChromeDriver(options);
-        }
+//        String browser = System.getProperties().getProperty("browser");
+//        if ((browser == null) || (browser.equals("chrome"))) {
+//            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
+//            driver = new ChromeDriver();
+//        } else if (browser.equals("yandex")) {
+//            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver108forya.exe");
+//            ChromeOptions options = new ChromeOptions();
+//            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+//            driver = new ChromeDriver(options);
+//        }
+        System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
+        driver = new ChromeDriver();
         return driver;
     }
 }
