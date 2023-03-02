30e201008856cb66360be850ef3222daaa2b81fc
Alexey Sysoev <wowregfatgod@yandex.ru>
2023-03-02 11:01:44 +0300

    trying run webdriver in jenkins v2

--- src/main/java/WebDriverSet.java
+++ src/main/java/WebDriverSet.java
@@ -6,18 +6,18 @@ public class WebDriverSet {
     private WebDriver driver;
 
     public WebDriver getWebDriver() {
-//        String browser = System.getProperties().getProperty("browser");
-//        if ((browser == null) || (browser.equals("chrome"))) {
-//            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
-//            driver = new ChromeDriver();
-//        } else if (browser.equals("yandex")) {
-//            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver108forya.exe");
-//            ChromeOptions options = new ChromeOptions();
-//            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
-//            driver = new ChromeDriver(options);
-//        }
-        System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
-        driver = new ChromeDriver();
+        String browser = System.getProperties().getProperty("browser");
+        if ((browser == null) || (browser.equals("chrome"))) {
+            //System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "C:/Users/Alexoid/javaprojects/diplom/Diplom_3/src/test/webdriver/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equals("yandex")) {
+            //System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver108forya.exe");
+            System.setProperty("webdriver.chrome.driver", "C:/Users/Alexoid/javaprojects/diplom/Diplom_3/src/test/chromedriver108ya.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("C:/Users/Alexoid/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+            driver = new ChromeDriver(options);
+        }
         return driver;
     }
 }
